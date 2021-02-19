60a93a6d28be73d91c1ab5b5718f7a7307fbf058
dan <danspyer@gmail.com>
2021-02-19 19:17:37 +0000

    fixed issue with react use query

--- src/Components/Home/NextLaunchLogic.tsx
+++ src/Components/Home/NextLaunchLogic.tsx
@@ -1,29 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { NextLaunchModel } from "./Interface/NextLaunchModel";
 import {NextLaunch} from './NextLaunch'
-import { useQuery, QueryResult } from "react-query";
+import { useQuery } from "react-query";
 
 export const NextLaunchLogic = (props: any) => {
+    function FetchLaunchData() {
+      return useQuery<NextLaunchModel, Error>("todos", async () => {
+        const response = await fetch("https://api.spacexdata.com/v4/launches/next");
+    
+        if (!response.ok) {
+          throw new Error("Failed to fetch!");
+        }
+    
+        return await response.json()
+      });
+    }
 
-    const [LaunchDate, updateLaunchDate] = useState("");
-    const [ShowData, setShowData] = useState(false);
-    const [Success, setSuccess] = useState(false)
-
-    function FetchLaunchData(): QueryResult<NextLaunchModel> {
-        return useQuery("todos", async () => {
-          const response = await fetch("https://api.spacexdata.com/v4/launches/next");
-      
-          if (!response.ok) {
-            throw new Error("Failed to fetch!");
-          }
-      
-          return await response.json();
-        });
-      }
-
-    const result = FetchLaunchData();
-
-    let year:any = new Date().getFullYear();    
+    const result = FetchLaunchData();  
 
     return (
       <NextLaunch show={result.status == "success"} data={result.data} />
