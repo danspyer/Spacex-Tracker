782d99a0a65685f21463088905ec63c88ef8ecd9
dan <danspyer@gmail.com>
2021-02-22 21:48:26 +0000

    moved the font page to use a bootstrap container

--- src/Components/Home/NextLaunch.tsx
+++ src/Components/Home/NextLaunch.tsx
@@ -11,21 +11,24 @@ export const NextLaunch = (props: NextLaunchProps) => {
     return (
         <div className="NextLaunchContainer LaunchDate">
             <div className="FlexContainer-H h100">
-                <div className="FlexChild h100 TextCenter LaunchDescription">
-                    <div className="LeftAlignText">
-                        <h4> UPCOMING LAUNCH: </h4>
-                        <h2 className="bold">{props?.data?.name}</h2>
-                    </div>
-                    <div className="LeftAlignText mt-2">
-                        <button className="btn SpaceXBtn">
-                            View More
-                        </button>
-                    </div>
-                
-                </div>
-                <div className="FlexChild h100 TextCenter">
-                    <div className="LeftAlignText">
-                        <CountdownTimer target_date_unix={props.data?.date_unix} update_interval={1000} />
+                <div className="FlexChild h100 TextCenter container-fluid">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="LeftAlignText">
+                                <h4> UPCOMING LAUNCH: </h4>
+                                <h2 className="bold">{props?.data?.name}</h2>
+                            </div>
+                            <div className="LeftAlignText mt-2">
+                                <button className="btn SpaceXBtn">
+                                    View More
+                                </button>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="LeftAlignText">
+                                <CountdownTimer target_date_unix={props.data?.date_unix} update_interval={1000} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
